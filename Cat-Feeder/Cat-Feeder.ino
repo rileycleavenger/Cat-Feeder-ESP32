d0bd263b578543444d6573c96bf5a63369dad814
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-01-11 13:21:26 -0500

    added base code

--- Cat-Feeder/Cat-Feeder.ino
+++ Cat-Feeder/Cat-Feeder.ino
@@ -1,9 +1,92 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
 
+// Set GPIOs for LED and reedswitch
+const int reedSwitch = 4;
+const int led = 2; //optional
+
+// Detects whenever the switch changed state
+bool changeState = false;
+
+// Holds reedswitch state (1=opened, 0=close)
+bool state;
+String switchState;
+
+// Auxiliary variables (it will only detect changes that are 1500 milliseconds apart)
+unsigned long previousMillis = 0; 
+const long interval = 1500;
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Initialize Telegram BOT
+#define BOTtoken "XXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "XXXXXXXXXX"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+// Runs whenever the reedswitch changes state
+ICACHE_RAM_ATTR void changeDoorStatus() {
+  Serial.println("State changed");
+  changeState = true;
+}
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);  
+
+  // Read the current switch state
+  pinMode(reedSwitch, INPUT_PULLUP);
+  state = digitalRead(reedSwitch);
+
+  // Set LED state to match door state
+  pinMode(led, OUTPUT);
+  digitalWrite(led, !state);
+  
+  // Set the reedswitch pin as interrupt, assign interrupt function and set CHANGE mode
+  attachInterrupt(digitalPinToInterrupt(reedSwitch), changeDoorStatus, CHANGE);
+
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (changeState){
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      // If a state has occured, invert the current switch  
+        state = !state;
+        if(state) {
+          switchState = "closed";
+        }
+        else{
+          switchState = "open";
+        }
+        digitalWrite(led, !state);
+        changeState = false;
+        Serial.println(state);
+        Serial.println(switchState);
+        
+        //Send notification
+        bot.sendMessage(CHAT_ID, "Cici was just fed!");
+    }  
+  }
 }
