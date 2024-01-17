86bdf906fbd7256fde505bd15fa5e50d5cb1de8d
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-01-17 10:35:39 -0500

    final version with keys removed

--- Cat-Feeder/Cat-Feeder.ino
+++ Cat-Feeder/Cat-Feeder.ino
@@ -3,55 +3,31 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
-// Set GPIOs for LED and reedswitch
-const int reedSwitch = 4;
-const int led = 2; //optional
+const int switchPin = 23;  // pin connected to the switch
+volatile int switchState = LOW;
 
-// Detects whenever the switch changed state
-bool changeState = false;
-
-// Holds reedswitch state (1=opened, 0=close)
-bool state;
-String switchState;
-
-// Auxiliary variables (it will only detect changes that are 1500 milliseconds apart)
-unsigned long previousMillis = 0; 
-const long interval = 1500;
-
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+// wifi info
+const char* ssid = "";
+const char* password = "";
 
 // Initialize Telegram BOT
-#define BOTtoken "XXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
+#define BOTtoken ""  // your Bot Token (Get from Botfather)
 
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define CHAT_ID "XXXXXXXXXX"
+#define RILEY_CHAT_ID ""
+#define COLIN_CHAT_ID ""
+#define TYLER_CHAT_ID ""
+#define MIKE_CHAT_ID ""
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
-// Runs whenever the reedswitch changes state
-ICACHE_RAM_ATTR void changeDoorStatus() {
-  Serial.println("State changed");
-  changeState = true;
-}
-
 void setup() {
-  // Serial port for debugging purposes
-  Serial.begin(115200);  
-
-  // Read the current switch state
-  pinMode(reedSwitch, INPUT_PULLUP);
-  state = digitalRead(reedSwitch);
-
-  // Set LED state to match door state
-  pinMode(led, OUTPUT);
-  digitalWrite(led, !state);
-  
-  // Set the reedswitch pin as interrupt, assign interrupt function and set CHANGE mode
-  attachInterrupt(digitalPinToInterrupt(reedSwitch), changeDoorStatus, CHANGE);
+  // baud rate
+  Serial.begin(115200);
+  pinMode(switchPin, INPUT_PULLUP);
 
   // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
@@ -64,29 +40,26 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");  
 
-  bot.sendMessage(CHAT_ID, "Bot started up", "");
+  bot.sendMessage(RILEY_CHAT_ID, "Bot started up", "");
 }
 
 void loop() {
-  if (changeState){
-    unsigned long currentMillis = millis();
-    if(currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-      // If a state has occured, invert the current switch  
-        state = !state;
-        if(state) {
-          switchState = "closed";
-        }
-        else{
-          switchState = "open";
-        }
-        digitalWrite(led, !state);
-        changeState = false;
-        Serial.println(state);
-        Serial.println(switchState);
-        
-        //Send notification
-        bot.sendMessage(CHAT_ID, "Cici was just fed!");
-    }  
+
+  int currentState = digitalRead(switchPin);
+  // check if the state has changed
+  if (currentState != switchState) {
+    switchState = currentState;
+
+    // print the message when the switch state changes
+    Serial.println("Switch state changed");
+
+    // send notification
+    bot.sendMessage(RILEY_CHAT_ID, "Cici was just fed!");
+    bot.sendMessage(COLIN_CHAT_ID, "Cici was just fed!");
+    bot.sendMessage(MIKE_CHAT_ID, "Cici was just fed!");
+    bot.sendMessage(TYLER_CHAT_ID, "Cici was just fed!");
   }
+
+  delay(1500); // delay for debouncing
 }
+
